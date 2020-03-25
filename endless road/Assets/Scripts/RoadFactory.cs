f5e79e7dc4b3db2348ee5d91117523f8f98ee485
stefan1234567891011 <s.hobeijn@hotmail.com>
2020-03-25 21:55:10 +0100

    added endless road project

---
+++ endless road/Assets/Scripts/RoadFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadFactory : MonoBehaviour
+{
+    public GameObject Prototype;
+    public Transform Spawnpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BlockSpawn());
+    }
+
+    IEnumerator BlockSpawn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Instantiate(Prototype, Spawnpoint.position, Spawnpoint.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
