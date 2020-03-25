86e55fd988f3e25cc42f1cad22b63ada3d5c3654
stefan1234567891011 <s.hobeijn@hotmail.com>
2020-03-25 21:46:05 +0100

    added Healthbar project

---
+++ Healthbar/Assets/Scripts/HealthBarHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class HealthBarHandler : MonoBehaviour
+{
+    private static Image HealthBarImage;
+
+    public PlayerController playerController;
+
+    public void OnEnable()
+    {
+        playerController = GetComponent<PlayerController>();
+        playerController.EnemyTouchedEvent.AddListener(EnemyTouchedEvent);
+    }
+
+    public void OnDisable()
+    {
+        playerController.EnemyTouchedEvent.RemoveListener(EnemyTouchedEvent);
+    }
+
+    /// <summary>
+    /// Sets the health bar value
+    /// </summary>
+    /// <param name="value">should be between 0 to 1</param>
+    public static void SetHealthBarValue(float value)
+    {
+        HealthBarImage.fillAmount = value;
+        if (HealthBarImage.fillAmount < 0.2f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if (HealthBarImage.fillAmount < 0.4f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+    }
+
+    public static float GetHealthBarValue()
+    {
+        return HealthBarImage.fillAmount;
+    }
+
+    /// <summary>
+    /// Sets the health bar color
+    /// </summary>
+    /// <param name="healthColor">Color </param>
+    public static void SetHealthBarColor(Color healthColor)
+    {
+        HealthBarImage.color = healthColor;
+    }
+
+    /// <summary>
+    /// Initialize the variable
+    /// </summary>
+    private void Start()
+    {
+        HealthBarImage = GetComponent<Image>();
+        SetHealthBarValue(1);
+    }
+
+    public void EnemyTouchedEvent()
+    {
+        SetHealthBarValue(GetHealthBarValue() - 0.1f);
+        if (GetHealthBarValue() <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Healthbar/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    //public float speed;
+
+    //private Rigidbody2D rb;
+
+    public UnityEvent EnemyTouchedEvent = new UnityEvent();
+
+    private void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x += 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.y += 0.1f;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            position.y -= 0.1f;
+            this.transform.position = position;
+        }
+    }
+
+    /*
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    */
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        EnemyTouchedEvent.Invoke();
+    }
+}
